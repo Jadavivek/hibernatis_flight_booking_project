d39aa402c6cd68d94b75af905bd80d90a559f506
Jadavivek <126766074+Jadavivek@users.noreply.github.com>
2026-03-24 00:52:39 +0530

    Update BookingService.java

--- src/main/java/com/example/flightbooking/repository/BookingService.java
+++ src/main/java/com/example/flightbooking/repository/BookingService.java
@@ -1,3 +1,6 @@
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
+package com.example.flightbooking.repository;
+
+import com.example.flightbooking.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {}
