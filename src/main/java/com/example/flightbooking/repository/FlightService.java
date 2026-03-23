14a138d9cadb64255b04f6184cf398570150fe62
Jadavivek <126766074+Jadavivek@users.noreply.github.com>
2026-03-24 00:53:36 +0530

    Update FlightService.java

--- src/main/java/com/example/flightbooking/repository/FlightService.java
+++ src/main/java/com/example/flightbooking/repository/FlightService.java
@@ -1,3 +1,6 @@
-@Repository
-public interface FlightRepository extends JpaRepository<Flight, Long> {
-}
+package com.example.flightbooking.repository;
+
+import com.example.flightbooking.entity.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlightRepository extends JpaRepository<Flight, Long> {}
